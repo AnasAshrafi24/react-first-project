a0fcbc95efce8c238f6cef7c16b3cb319c6400d2
AnasAshrafi24 <Anas24ashrafi@gmail.com>
2020-09-30 23:58:36 -0700

    React Chap App

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Header from './components/Header'
+import Footer from './components/Footer'
 import './App.css';
+import BasicTextFields from './components/Text'
+import Navbar from './components/Navbar';
+import GridList from './components/GridList';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+class App extends Component {
+  render() {
+    return(
+      <div style={{backgroundColor:'#5f8a6a'}}>
+        <Navbar/>
+        <Header/>
+       
+     
+         
+        <BasicTextFields/>
+        <GridList/>
+        <Footer/>
+      </div>
+    )
+  }
 }
 
 export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+class Haeder extends React.Component{
+    render(){
+        return(
+            <div className="header">
+      
+      <div id="carouselExampleSlidesOnly" className="carousel slide" data-ride="carousel">
+        <div className="carousel-inner">
+          <div className="carousel-item active">
+            <img src="https://www.smartertravel.com/uploads/2017/04/shutterstock_600664103-1400x500.jpg" className="d-block w-100" alt="..." />
+          </div>
+         
+        </div>
+      </div>
+
+            </div>
+        )
+    }
+}
+
+export default Haeder ;
